c5f643f7537f29bffdabf6155b6eca5b4107d203
wladi <wlad@>
2023-11-09 15:50:01 +0300

    initialization and display str

--- main.java
+++ main.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        String str = scanner.nextLine();
+        System.out.println(str);
     }
 }
